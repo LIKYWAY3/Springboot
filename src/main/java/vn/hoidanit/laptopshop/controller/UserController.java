15362e7a4d3dbfdd56a01c151e694fb51f9a922f
LIKYWAY3 <nguyenhuynhkha0203@gmail.com>
2026-03-29 17:26:12 +0700

    xong send post form

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -4,41 +4,44 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
-// MÔ hình mvc
+//Baitap 1
 @Controller
 public class UserController {
-    private UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+    @RequestMapping("/admin/user")
+    public String getPageUser(Model model) {
+        model.addAttribute("newUsers", new User());
+        return "admin/user/create";
     }
 
-    @RequestMapping("/")
-    public String getHomePage(Model model) {
-        String test = userService.handleHello();
-        model.addAttribute("khajsp", test);
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String getPostPage(Model model, @ModelAttribute("newUsers") User kha2) {
+        System.out.println("Run here" + kha2);
         return "hello";
     }
 }
 
-// Mô hình Restfull APIs
-// @RestController
+// MÔ hình mvc
+// @Controller
 // public class UserController {
-
 // private UserService userService;
 
 // public UserController(UserService userService) {
 // this.userService = userService;
 // }
 
-// @GetMapping("")
-// public String getHomePage() {
-// return this.userService.handleHello();
+// @RequestMapping("/")
+// public String getHomePage(Model model) {
+// String test = userService.handleHello();
+// model.addAttribute("khajsp", test);
+// return "hello";
 // }
-
 // }
