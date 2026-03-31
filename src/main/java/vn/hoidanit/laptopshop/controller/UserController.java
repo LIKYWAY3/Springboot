0aec8dfa295ed1b2fb9057feb6a4357c0c80fd63
LIKYWAY3 <nguyenhuynhkha0203@gmail.com>
2026-03-31 22:39:27 +0700

    sql query

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,11 +12,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //Baitap 1
 @Controller
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+
+    }
+
+    @RequestMapping("/")
+    public String Home(Model model) {
+        List<User> arUsers = this.userService.getUserbyEmail("hai@gmail.com");
+        System.out.println(arUsers);
+        return "hello";
+    }
 
     @RequestMapping("/admin/user")
     public String getPageUser(Model model) {
@@ -25,6 +42,7 @@ public class UserController {
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String getPostPage(Model model, @ModelAttribute("newUsers") User kha2) {
         System.out.println("Run here" + kha2);
+        this.userService.handleSave(kha2);
         return "hello";
     }
 }
